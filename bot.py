ca9746ba1f2cace3069a59b8edc34f5f0f9b021f
Alireza <armanparsi206@outlook.com>
2023-04-09 16:12:57 +0330

    Basic code

--- bot.py
+++ bot.py
@@ -1,3 +1,7 @@
+# Python 3.10
+# Developer : Github.com/alireza536
+
+# Importing ... 
 from telegram.ext import Updater
 from telegram.ext import CallbackContext
 from telegram.ext import CommandHandler
@@ -11,3 +15,19 @@ from telegram import ReplyKeyboardMarkup
 
 from time import sleep , time
 
+
+
+
+
+# Main
+if __name__ == '__main__' :
+    # Get API Token
+    TOKEN = open('Token.txt' , 'r').readline()
+    
+    # Updater
+    updater = Updater(TOKEN)
+
+
+    # Start Bot
+    updater.start_polling()
+    updater.idle()
