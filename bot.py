733156ee5f28070dfa00b51979e7da4bc729a4f6
Alireza <armanparsi206@outlook.com>
2023-04-09 15:52:02 +0330

    Import

---
+++ bot.py
@@ -0,0 +1,13 @@
+from telegram.ext import Updater
+from telegram.ext import CallbackContext
+from telegram.ext import CommandHandler
+from telegram.ext import MessageFilter
+from telegram.ext import Filters
+
+from telegram.chataction import ChatAction
+
+from telegram import Update
+from telegram import ReplyKeyboardMarkup
+
+from time import sleep , time
+
